845946237f81813d2dee5ba715a2322870c92813
Ramon Cervera <rcerver4@xtec.cat>
2024-01-08 19:40:55 +0100

    first commit

--- oca-moduls/src/main/java/com/daw1/Main.java
+++ oca-moduls/src/main/java/com/daw1/Main.java
@@ -3,6 +3,15 @@ package com.daw1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Joc de l'oca en mòduls DAW1");
+
+        // Laberint de 63 caselles
+        String[] laberint = new String[63];
+       
+        // jugadors: 4 jugadors, 
+            //columna 0: casella on està jugador dins del laberint
+            //columna 1: número torns pendents de no tirar
+        int[][] jugadors = new int[4][2]; // tot 0 inicialment per defecte!
+        int numJugador = 1; // comença el jugador 0   
       
     }
 }
