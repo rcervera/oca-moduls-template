c03617116d25521e148d2832c96e86d21494a550
Ramon Cervera <rcerver4@xtec.cat>
2024-01-08 14:50:43 +0100

    first commit

---
+++ oca-moduls/src/main/java/com/daw1/Main.java
@@ -0,0 +1,8 @@
+package com.daw1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Joc de l'oca en mòduls DAW1");
+        Modul1.funcio1();
+    }
+}
---
+++ oca-moduls/src/main/java/com/daw1/Modul1.java
@@ -0,0 +1,8 @@
+package com.daw1;
+
+public class Modul1 {
+    
+    public static void funcio1() {
+
+    }
+}
